b22b6340928d390409091ce3995cb2e11917ed5f
Kevin williams <kevinity310@outlook.com>
2021-01-18 23:53:37 +0700

    Membuat atau menyimpan sesi Login [Session & Intent]

--- app/src/main/java/com/example/androidsharepreferenceslazday/MainActivity.kt
+++ app/src/main/java/com/example/androidsharepreferenceslazday/MainActivity.kt
@@ -1,11 +1,59 @@
 package com.example.androidsharepreferenceslazday
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.androidsharepreferenceslazday.helper.Constant
+import com.example.androidsharepreferenceslazday.helper.PreferencesHelper
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    private lateinit var  sharedPref: PreferencesHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) { // apapun yang ada di onCreate akan dijaankan
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sharedPref = PreferencesHelper(this)
+
+        buttonLogin.setOnClickListener {
+            if (editUsername.text.isNotEmpty() && editUsername.text.isNotEmpty()){
+                saveSession(editUsername.text.toString(), editPassword.text.toString())
+                shortMassage("Berhasil Masuk")
+                moveIntent()
+
+                //sharedPref.put( Constant.PREF_USERNAME, editUsername.text.toString()) // put digunakan untuk menyimpan data
+                //sharedPref.put( Constant.PREF_PASSWORD, editPassword.text.toString())
+                //sharedPref.put( Constant.PREF_IS_LOGIN, true)
+            }
+        }
     }
+
+    private fun saveSession ( username: String, password: String){
+        sharedPref.put( Constant.PREF_USERNAME, editUsername.text.toString()) // put digunakan untuk menyimpan data
+        sharedPref.put( Constant.PREF_PASSWORD, editPassword.text.toString())
+        sharedPref.put( Constant.PREF_IS_LOGIN, true)
+    }
+
+
+    // menggunakan onStart untuk melakukan Pengecekan
+    override fun onStart() {
+        super.onStart()
+        if (sharedPref.getBoolean(Constant.PREF_IS_LOGIN) == true) {
+            moveIntent()
+        }
+    }
+
+    private fun moveIntent(){
+        startActivity(Intent(this, UserActivity::class.java))
+    }
+
+
+    // Pesan singkat login telah berhasil
+    private fun shortMassage(massage: String){
+        Toast.makeText(applicationContext, massage, Toast.LENGTH_SHORT).show()
+    }
+
 }
--- app/src/main/java/com/example/androidsharepreferenceslazday/helper/Constant.kt
+++ app/src/main/java/com/example/androidsharepreferenceslazday/helper/Constant.kt
@@ -1,7 +1,10 @@
 package com.example.androidsharepreferenceslazday.helper
 
 class Constant { // data"yang dibutuhkan key ada disini
-    val PREF_IS_LOGIN   = "PREF_IS_LOGIN" // nilai pakai Boolean, untuk mendeteksi sudah Login atau belum
-    val PREF_USERNAME   = "PREF_USERNAME" // menyimpan username
-    val PREF_PASSWORD   = "PREF_PASSWORD" // menyimpan password
+
+companion object { // tanpa companion object, makan akan bermasalah di class main
+    val PREF_IS_LOGIN = "PREF_IS_LOGIN" // nilai pakai Boolean, untuk mendeteksi sudah Login atau belum
+    val PREF_USERNAME = "PREF_USERNAME" // menyimpan username
+    val PREF_PASSWORD = "PREF_PASSWORD" // menyimpan password
+    }
 }
