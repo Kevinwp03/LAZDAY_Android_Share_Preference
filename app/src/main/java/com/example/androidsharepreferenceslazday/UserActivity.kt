336e33f91e3d51d6b82f417dfc04353c197429cf
Kevin williams <kevinity310@outlook.com>
2021-01-18 21:03:18 +0700

    make desain login and logout

--- app/src/main/java/com/example/androidsharepreferenceslazday/UserActivity.kt
+++ app/src/main/java/com/example/androidsharepreferenceslazday/UserActivity.kt
@@ -1,11 +1,11 @@
-package com.example.androidsharepreference
+package com.example.androidsharepreferenceslazday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class UserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_user)
     }
 }
