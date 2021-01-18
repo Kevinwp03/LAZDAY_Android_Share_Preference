aa45d1dd6d94023cd3de0a3cb0f2bb22df102d9b
Kevin williams <kevinity310@outlook.com>
2021-01-18 22:41:14 +0700

    Membuat Class Helper

---
+++ app/src/main/java/com/example/androidsharepreferenceslazday/helper/Constant.kt
@@ -0,0 +1,7 @@
+package com.example.androidsharepreferenceslazday.helper
+
+class Constant { // data"yang dibutuhkan key ada disini
+    val PREF_IS_LOGIN   = "PREF_IS_LOGIN" // nilai pakai Boolean, untuk mendeteksi sudah Login atau belum
+    val PREF_USERNAME   = "PREF_USERNAME" // menyimpan username
+    val PREF_PASSWORD   = "PREF_PASSWORD" // menyimpan password
+}
---
+++ app/src/main/java/com/example/androidsharepreferenceslazday/helper/PreferencesHelper.kt
@@ -0,0 +1,36 @@
+package com.example.androidsharepreferenceslazday.helper
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class PreferencesHelper (context: Context){
+    private val PREFS_NAME = "sharedprefkotlin12345" // di rekomendasi harus ada penamaan agar tidak bertabrakan dengan data yang lain
+    private val sharedpref: SharedPreferences // share Preferences membutuhkan context
+    val editor: SharedPreferences.Editor
+
+    init { // ketika kelas helper ini dipanggil secara otomatis yg terdapat dalam init akan dijalankan
+        sharedpref = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        editor = sharedpref.edit()
+    }
+    // cara menyimpan data pada Share Prefenrences
+    fun put(key: String, value: String){
+        editor.putString(key, value) // kunci dan value akan disimpan di line ini
+            .apply()
+    }
+
+    // Untuk mendapatkan nilainya
+    fun getString(key: String) : String? {
+        return sharedpref.getString(key, null) // ini default value, jadi misalkan keynya belum ada maka valuenya akan diisi data null
+    }
+
+    // Bagaimana misalnya nilainya INT atau Boolean?
+    fun put(key: String, value: Boolean){
+        editor.putBoolean(key, value) // kunci dan value akan disimpan di line ini
+            .apply()
+    }
+
+    // Untuk mendapatkan nilainya
+    fun getBoolean(key: String) : Boolean? {
+        return sharedpref.getBoolean(key, false) // ini default value, jadi misalkan keynya belum ada maka valuenya akan diisi data null
+    }
+}
