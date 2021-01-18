c97b0e8dc2de3d31992ca0656901b0a02ad6ffba
Kevin williams <kevinity310@outlook.com>
2021-01-19 00:42:06 +0700

    Menampilkan data & Membuat Logout

--- app/src/main/java/com/example/androidsharepreferenceslazday/UserActivity.kt
+++ app/src/main/java/com/example/androidsharepreferenceslazday/UserActivity.kt
@@ -1,11 +1,38 @@
 package com.example.androidsharepreferenceslazday
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.androidsharepreferenceslazday.helper.Constant
+import com.example.androidsharepreferenceslazday.helper.PreferencesHelper
+import kotlinx.android.synthetic.main.activity_user.*
 
 class UserActivity : AppCompatActivity() {
+
+    lateinit var sharedpref: PreferencesHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
+
+        sharedpref = PreferencesHelper(this)
+        textUsername.text = sharedpref.getString( Constant. PREF_USERNAME ) // untuk mendapatkan data username
+
+        buttonLogout.setOnClickListener {
+            sharedpref.clear()
+            shortMassage("Keluar")
+            moveIntent()
+        }
+
     }
+
+    private fun moveIntent(){
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+
+    private fun shortMassage(massage: String){
+        Toast.makeText(applicationContext, massage, Toast.LENGTH_SHORT).show()
+    }
+
 }
--- app/src/main/java/com/example/androidsharepreferenceslazday/helper/PreferencesHelper.kt
+++ app/src/main/java/com/example/androidsharepreferenceslazday/helper/PreferencesHelper.kt
@@ -6,7 +6,8 @@ import android.content.SharedPreferences
 class PreferencesHelper (context: Context){
     private val PREFS_NAME = "sharedprefkotlin12345" // di rekomendasi harus ada penamaan agar tidak bertabrakan dengan data yang lain
     private val sharedpref: SharedPreferences // share Preferences membutuhkan context
-    val editor: SharedPreferences.Editor
+
+    val editor: SharedPreferences.Editor // semua data disimpan disini
 
     init { // ketika kelas helper ini dipanggil secara otomatis yg terdapat dalam init akan dijalankan
         sharedpref = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
@@ -33,4 +34,11 @@ class PreferencesHelper (context: Context){
     fun getBoolean(key: String) : Boolean? {
         return sharedpref.getBoolean(key, false) // ini default value, jadi misalkan keynya belum ada maka valuenya akan diisi data null
     }
+
+    // menghapus data di sharePreference saat Logout
+    fun clear() {
+        editor.clear()
+            .apply()
+    }
+
 }
